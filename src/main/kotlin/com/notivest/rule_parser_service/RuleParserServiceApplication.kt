b9e764de6d976f644b8dca5dca21e5343a8639c2
TaisMarambio <gonzalo.mato@ing.austral.edu.ar>
2025-05-22 18:58:21 -0300

    first commit

---
+++ src/main/kotlin/com/notivest/rule_parser_service/RuleParserServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.notivest.rule_parser_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RuleParserServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<RuleParserServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/notivest/rule_parser_service/RuleParserServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.notivest.rule_parser_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RuleParserServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
